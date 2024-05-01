5a3c1c6fd43e820b83641051f26204616e4f3cc9
Megan Chow <megchow@umich.edu>
2024-05-01 16:54:56 -0400

    added token gen

---
+++ generate_token.sh
@@ -0,0 +1,3 @@
+curl -X POST "https://accounts.spotify.com/api/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=c7d428f54ba349c08fa49ffe3d72172d&client_secret=94e25beef0cd44338c16faf04e206750"
